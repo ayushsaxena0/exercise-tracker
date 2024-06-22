1e98053925f392dfa6a1c9b16e2b5e8a427642db
Ayush <ayushsaxena917@gmail.com>
2024-06-22 19:39:11 +0530

    Exercise Tracker (FreeCodeCamp Project)

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const connectDB = require("./config/database");
+const cors = require("cors");
+const exerciseController = require("./controllers/exerciseController");
+require("dotenv").config({ path: "./config/.env" });
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+connectDB();
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-
-
+app.get("/api/users", exerciseController.getUsers);
+app.post("/api/users", exerciseController.postUser);
+app.post("/api/users/:_id/exercises", exerciseController.postExercise);
+app.get("/api/users/:_id/logs", exerciseController.getLog);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
